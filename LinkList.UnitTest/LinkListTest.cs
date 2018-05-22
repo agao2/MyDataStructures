5d4435c83e85703179e4407caa45b94e44cae71d
Alex <gaoalexander73@gmail.com>
2018-05-21 19:52:16 -0700

    LinkList unit tests
    48% blocks covered as of this commit

---
+++ LinkList.UnitTest/LinkListTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+using LinkList;
+
+namespace LinkList.UnitTest
+{
+    /**
+     * This test class tests adding items to the link list. 
+     * All test method names have the following format:
+     *  [methodName]_[scenario]_[expectedBehavior]
+     * */
+
+    [TestFixture]
+    public class LinkListTest
+    {
+        [Test]
+        public void LinkList_NewInitialization_NotNull()
+        {
+            LinkList newList = new LinkList();
+            Assert.NotNull(newList);
+        }
+
+        [Test]
+        public void LinkedList_ExistingInitialization_NotNull()
+        {
+            LLNode head = new LLNode();
+            LLNode tail = new LLNode();
+            head.Next = tail;
+
+            LinkList newList = new LinkList(head, tail);
+            Assert.NotNull(newList);
+        }
+
+        [Test]
+        public void LinkList_NullHead_ArgumentNullException()
+        {
+            LLNode tail = new LLNode();
+            Assert.Throws<ArgumentNullException>(() => new LinkList(null, tail));
+        }
+
+        [Test]
+        public void LinkList_NullTail_ArgumentNullException()
+        {
+            LLNode head = new LLNode();
+            Assert.Throws<ArgumentNullException>(() => new LinkList(head, null));
+        }
+
+        [Test]
+        public void LinkList_TailNextNotNull_ArgumentException()
+        {
+            LLNode head = new LLNode();
+            LLNode tail = new LLNode
+            {
+                Next = head
+            };
+
+            Assert.Throws<ArgumentException>(() => new LinkList(head, tail));
+        }
+
+        [Test]
+        public void LinkList_UnconnectedHeadTail_ArgumentException()
+        {
+            LLNode head = new LLNode();
+            LLNode tail = new LLNode();
+
+            Assert.Throws<ArgumentException>(() => new LinkList(head, tail));
+        }
+    }
+}
